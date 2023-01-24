e92cebf59d57f23bd9ea3324ac9479859ca300f8
Kita Igniter <smtecsystem.469@gmail.com>
2023-01-24 20:16:09 -0300

    Server Aula 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,11 @@
 import Fatisfy from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
 const app = Fatisfy();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get("/", async (req, res) => {
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-});
+app.register(appRoutes);
 
 app.listen({
   port: 3333,
