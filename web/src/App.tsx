802f3b961d3062c443c050e7ef1b6ceed061da94
Kita Igniter <smtecsystem.469@gmail.com>
2023-01-26 21:01:34 -0300

    Web Aula 2

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,14 @@
-import Habit from "./components/Habit"
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 import "./styles/global.css";
 
-function App() {
+export function App() {
   return (
-    <>
-      <Habit completed={20}/> 
-      <Habit completed={30}/> 
-      <Habit completed={40}/> 
-      <Habit completed={50}/> 
-      <Habit completed={60}/> 
-    </>  
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-x-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable/>
+      </div>       
+    </div>  
   )
 }
-
-export default App
